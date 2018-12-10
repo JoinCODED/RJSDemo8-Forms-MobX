1c4ac289f83d97abc1018a9072f7038388d1b4c0
Lailz <lailaabdulraheem@gmail.com>
2018-12-10 11:40:28 +0300

    ready

--- src/store/AliasStore.js
+++ src/store/AliasStore.js
@@ -2,8 +2,8 @@ import { observable, decorate } from "mobx";
 import axios from "axios";
 
 class AliasStore {
+  data = [];
   statusMessage = "";
-  aliases = [];
 
   fetchList = async () => {
     try {
@@ -13,6 +13,17 @@ class AliasStore {
       console.error(err);
     }
   };
+
+  postForm(data) {
+    axios
+      .post("http://127.0.0.1:8000/alias/", data)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(err.response);
+      });
+  }
 }
 
 decorate(AliasStore, {
